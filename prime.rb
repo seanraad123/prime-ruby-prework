f3b00aef7ade30835cfb798a3aaaba4f466cbe0c
Sean Conrad <seanconrad123@gmail.com>
2018-06-02 03:06:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,11 +8,14 @@ def prime?(n)
        return true
     elsif n % 2 == 0 || n % 3 == 0
        return false
+    end
     i = 5
     while i * i <= n
        if n % i == 0 || n % (i + 2) == 0
            return false
        i += 6
     return true
+  end
+end
 
 print prime?(3)
