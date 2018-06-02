c0c9a03d49857d644fd8fe23efe3552734e69895
Sean Conrad <seanconrad123@gmail.com>
2018-06-02 03:41:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(n)
   output = (2..n).find { |i| n % i == 0 }
-  puts output
+  puts output ?
   output == n ? "#{n} is prime" : "#{n} is NOT prime"
 end
 
-puts prime?(3)
+prime?(3)
